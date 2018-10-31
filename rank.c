85106b818a85f3be86e4ccee0e0d0743fdb73e92
Orion <mathieu.durand@fr.ibm.com>
2018-10-31 18:34:57 +0100

    premiere variable d'entrée

--- rank.c
+++ rank.c
@@ -3,9 +3,10 @@
 
 int main ()
 {
+	char phrase[256];
 // strcpy(reponse,"ok");
  printf ("bonjour monde\n> ");
-// fgets(phrase,256,stdin);
+ fgets(phrase,256,stdin);
 // while (strstr(phrase,"quit")==NULL&&strstr(phrase,"au revoir")==NULL&&strstr(phrase,"bye")==NULL&&strstr(phrase,"fin")==NULL&&strstr(phrase,"end")==NULL)
 // {
 //  traite();
