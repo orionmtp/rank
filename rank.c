437a4f0358ac68f5e3688557b7ff725e06ec10c9
Orion <mathieu.durand@fr.ibm.com>
2018-10-31 18:31:46 +0100

    ca fait longtemps que j'ai pas fait de C
    premieres operations basiques d'IO

---
+++ rank.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+// strcpy(reponse,"ok");
+ printf ("bonjour monde\n> ");
+// fgets(phrase,256,stdin);
+// while (strstr(phrase,"quit")==NULL&&strstr(phrase,"au revoir")==NULL&&strstr(phrase,"bye")==NULL&&strstr(phrase,"fin")==NULL&&strstr(phrase,"end")==NULL)
+// {
+//  traite();
+//  printf("%s\n> ",reponse);
+//  fgets(phrase,1024,stdin);
+// }
+ printf("au revoir\n");
+ return(0);
+}
